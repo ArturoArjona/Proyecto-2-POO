02cf302e0599c15650da19d829ab9adc095e5168
ArturoArjona <61487786+ArturoArjona@users.noreply.github.com>
2020-04-24 16:29:11 -0500

    Update hora.h

--- hora.h
+++ hora.h
@@ -1,16 +1,17 @@
 #include <iostream>
 
+using namespace std;
 
 class Hora
 {
-  public
+  public:
   Hora();
   Hora(int h, int m);
   void setHh(int h) {hh=(h >= 0 && h<24 ? h : 0);};
   int getHh() {return hh;};
-  void setMm (int m) {mm=(m >= 0 && h<60 ? m : 0);};
-  string getMm() {return mm;};
+  void setMm (int m) {mm=(m >= 0 && m<60 ? m : 0);};
+  int getMm() {return mm;};
   void muestra();
-  private
+  private:
   int hh, mm;
 };
